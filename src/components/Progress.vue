c6e0c93a749d21bb72479896d775cc948b1cdec3
Bekir İstek <bekir.istek@gmail.com>
2021-07-23 15:54:16 +0300

    v1.0

---
+++ src/components/Progress.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar bg-info"
+      role="progressbar"
+      :style="{ width: progressBar + '%' }"
+      aria-valuenow="50"
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      {{ currentItem }}/10
+    </div>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main";
+export default {
+  data() {
+    return {
+      currentItem: 0,
+      progressBar: 0,
+    };
+  },
+
+  created() {
+    eventBus.$on("progressBarUpdated", (productListCount) => {
+      this.currentItem = productListCount;
+      this.progressBar = productListCount * 10;
+    });
+  },
+};
+</script>
